af5a264af50118ead2873eb0c313231fa537fe39
unknown <you@example.com>
2026-05-05 19:53:38 +0530

    update app.java

--- ai-projects/multi-agents-cicd-project/src/main/java/App.java
+++ ai-projects/multi-agents-cicd-project/src/main/java/App.java
@@ -2,10 +2,7 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello AI DevOps");
-=======
-        System.out.println("Hello AI DevOps");;;
->>>>>>> cef3ce401d67708f56fb4764a4623d430e02b9f1
+        System.out.println("Hello AI DevOps")
+
     }
 }
